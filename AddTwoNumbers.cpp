19cd8d09af02766512da3b386eee0026f864f5db
run502 <run502@github.com>
2017-04-04 23:52:16 +0800

    Leetcode 第二题

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -10,17 +10,29 @@
 #define LEN 0
 
 struct ListNode {
-    int val;
-    struct ListNode *next;
+    int val;                //值域 
+    struct ListNode *next;  //指针域 
 };
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     struct ListNode *l3;       //申请l3，用来存储l1和l2的和 
-    l3 = (ListNode *)malloc(sizeof(ListNode));    //申请一段 ListNode大小的内存
+    l3 = (struct ListNode *)malloc(sizeof(struct ListNode));    //申请一段 ListNode大小的内存
     l3->val = LEN;      //初始化l3的值为 0 
     l3->next = NULL;    //初始化l3的指针域为 NULL
 	struct ListNode *l3_back = l3;    //创建 l3的备份
 	static int carry = 0;             //设置进位, 0为没有进位, 1为有进位 
+	
+	if (l1 == NULL && l2 == NULL) {   //两个数均为空 
+		return NULL;                  //返回空 
+	} 
+	
+	if (l1 != NULL && l2 == NULL) {   // l2为空 
+		return l1;                    //返回 l1 
+	}
+	
+	if (l1 == NULL && l2 != NULL) {   // l1为空 
+		return l2;                    //返回 l2
+	}
     
     while (l1 != NULL & l2 != NULL) {
     	struct ListNode *p = NULL; 
@@ -35,21 +47,6 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
 			p->val = l1->val+l2->val+carry;         //p->val的值就是位数相加再加进位 
 			carry = 0;                              //进位恢复为 0 
 		}
-		
-		if (l1->next == NULL && l2->next == NULL) { //如果两个数位数相同 
-			if (l1->val+l2->val+carry >= 10) {      //且最后一位产生了进位 
-				struct ListNode *p = NULL;
-				p = (struct ListNode *)malloc(sizeof(struct ListNode));    //申请一段 ListNode大小的内存 
-				
-				p->val = 1;			//进位 1，将 p->val的值设置为1 
-				
-				p->next = l3->next;
-				l3->next = p;
-				l3 = l3->next;      //使得 l3->p->NULL 
-				
-				return l3_back->next;                 //返回需要的值
-			}
-		} 
 		
 		p->next = l3->next;                         //使p和l3指向同一个结点 NULL 
 		l3->next = p;                               //使得 l3->p->NULL 
@@ -68,7 +65,9 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
 		l3->next = p;
 		l3 = l3->next; 
 		
-		l1 = l1->next;              //使指针往后移动 
+		l1 = l1->next;              //使指针往后移动
+		
+		return l3_back->next;                 //返回需要的值 
 	}
 	
 	if (l2 != NULL) {         //如果 l2还没有循环完毕  
@@ -81,6 +80,19 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
 		l3 = l3->next;
 		
 		l2 = l2->next;              //使指针往后移动
+		
+		return l3_back->next;                 //返回需要的值
+	}
+	
+	if (carry == 1) {               //两个数位数相同且产生了进位 
+		struct ListNode *p = NULL;
+		p = (struct ListNode *)malloc(sizeof(struct ListNode));    //申请一段 ListNode大小的内存 
+				
+		p->val = 1;			//进位 1，将 p->val的值设置为1 
+				
+		p->next = l3->next;
+		l3->next = p;
+		l3 = l3->next;      //使得 l3->p->NULL 
 	}
 	
 	return l3_back->next;                 //返回需要的值
